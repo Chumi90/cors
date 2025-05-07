49fa1abd4000cff4ee432a924133576e91f3706e
Chumi90 <abel_chumillas@hotmail.com>
2025-05-07 23:43:22 +0200

    Commit Finish

--- front/code.js
+++ front/code.js
@@ -12,11 +12,27 @@ const data=async()=>{
             throw new Error ('Hay un error en la descarga',response.status);
         }
         const datas=await response.json();
-        console.log(datas[0]);
+        dataHtml(datas[0]);
+
     }
     catch(error){
         console.log('Error al obtener los datos',error)
     }
+}
+
+function dataHtml(personaje){
+    const {name, status, species,origin:{name1},image,gender}=personaje
+    results.innerHTML=`
+        <h3>El personaje obtenido es:</h3>
+        <ul>
+            <li>Name: ${name}</li>
+            <li>Status: ${status}</li>
+            <liSpecies: ${species}</li>
+            <li>Gender: ${gender}</li>
+            <li>Origin: ${name1}</li>
+        </ul>
+        <img src="${image}">
+    `
 }
 
 btn.addEventListener("click",()=>{
